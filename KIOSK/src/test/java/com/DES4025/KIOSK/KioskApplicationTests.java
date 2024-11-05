e708b651f2fcd8e42e42a3aad844cc33b8228099
anismynm <ddoe0318@naver.com>
2024-11-05 18:11:41 +0900

    feat: 최초 등록

---
+++ KIOSK/src/test/java/com/DES4025/KIOSK/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DES4025.KIOSK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
