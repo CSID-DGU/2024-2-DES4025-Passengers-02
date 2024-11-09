7ae00383e5708923826aa91aed0b38cd8617b02a
anismynm <ddoe0318@naver.com>
2024-11-10 03:42:08 +0900

    feat: 장바구니 수정 기능, 주문 확정 기능

--- KIOSK/src/main/java/DES4025/KIOSK/Controller/CartController.java
+++ KIOSK/src/main/java/DES4025/KIOSK/Controller/CartController.java
@@ -1,5 +1,6 @@
 package DES4025.KIOSK.Controller;
 
+import DES4025.KIOSK.DTO.modifyCartDTO;
 import DES4025.KIOSK.Entity.OrderDetail;
 import DES4025.KIOSK.Service.CartService;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @Slf4j
@@ -22,8 +20,8 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping("/{order-num}/cart")
-    public ResponseEntity<Map<String, Object>> getCart(@PathVariable("order-num") Integer order_num) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<?> getCart(@PathVariable("order-num") Integer order_num) {
+        Map<String, Object> response = new LinkedHashMap<>();
 
         try {
             List<OrderDetail> orderList = cartService.getCart(order_num);
@@ -38,6 +36,46 @@ public class CartController {
                 response.put("order", orderList);
                 return ResponseEntity.ok(response);
             }
+        } catch (Exception e) {
+            response.put("code", "Error");
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping("/{order-num}/cart/modify")
+    public ResponseEntity<?> modifyCart(modifyCartDTO modifyCartDTO) {
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        try {
+            OrderDetail orderDetail = cartService.modifyCart(modifyCartDTO);
+            response.put("code", "SU");
+            response.put("message", "Success.");
+            if (orderDetail != null) {
+                response.put("modify Detail", orderDetail);
+            }
+            else {
+                response.put("Deleted Order Detail Num", modifyCartDTO.getOrder_detail_num());
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            response.put("code", "Error");
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping("/{order-num}/order-num")
+    public ResponseEntity<?> getOrderNum(@PathVariable("order-num") Integer order_num) {
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        try {
+            int total_price = cartService.getTotalPrice(order_num);
+            response.put("code", "SU");
+            response.put("message", "Success.");
+            response.put("order_num", order_num);
+            response.put("total_price", total_price);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         } catch (Exception e) {
             response.put("code", "Error");
             response.put("message", e.getMessage());
