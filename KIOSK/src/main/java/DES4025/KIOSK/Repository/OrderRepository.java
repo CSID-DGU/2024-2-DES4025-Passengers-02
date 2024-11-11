ba9f6f427feb3581573c8c574a5b8ffc89cbad7b
anismynm <ddoe0318@naver.com>
2024-11-11 17:11:10 +0900

    feat: front와의 통신 위해 수정 및 리팩토링

--- KIOSK/src/main/java/DES4025/KIOSK/Repository/OrderRepository.java
+++ KIOSK/src/main/java/DES4025/KIOSK/Repository/OrderRepository.java
@@ -1,17 +1,15 @@
 package DES4025.KIOSK.Repository;
 
 import DES4025.KIOSK.Entity.dOrder;
-import jakarta.persistence.Table;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<dOrder, Integer> {
-
-    @Query(value = "update dOrder set take_out_mode = :take_out_mode where order_num = :order_num", nativeQuery = true)
+    @Query(value = "update dOrder set orderDetail = :menu, total_price = :total_price where order_num = :order_num", nativeQuery = true)
     @Modifying
     @Transactional
-    void updateTakeOutMode(@Param("order_num") Integer order_num, @Param("take_out_mode") boolean take_out_mode);
+    void setMenu(@Param(value = "order_num") int order_num, @Param(value = "menu") String menu, @Param(value = "total_price") int total_price);
 }
