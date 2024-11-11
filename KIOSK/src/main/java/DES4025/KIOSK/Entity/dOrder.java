87d623565f5891509263a4774cf7caf517690300
anismynm <ddoe0318@naver.com>
2024-11-11 21:19:23 +0900

    feat: 프론트엔드 api 연결 설정, DB 수정

--- KIOSK/src/main/java/DES4025/KIOSK/Entity/dOrder.java
+++ KIOSK/src/main/java/DES4025/KIOSK/Entity/dOrder.java
@@ -1,10 +1,8 @@
 package DES4025.KIOSK.Entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
@@ -32,6 +30,7 @@ public class dOrder {
     @Column(name = "pay_status", nullable = false, columnDefinition = "boolean default false")
     private boolean payStatus;
 
-    @Column(name = "orderDetail")
+    @Lob
+    @Column(name = "orderDetail", columnDefinition = "TEXT")
     private String orderDetail;
 }
