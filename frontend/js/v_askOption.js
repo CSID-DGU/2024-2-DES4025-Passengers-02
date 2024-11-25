10c0846ef719991e4b485bf518612938cb0b4f52
yewoonn <hanyw0207@naver.com>
2024-11-25 22:32:21 +0900

    feat: 프론트 변경 사항 확인

--- frontend/js/v_askOption.js
+++ frontend/js/v_askOption.js
@@ -14,11 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
             // 선택된 옵션의 ID 가져오기
             const optionId = this.id;
 
+            if(optionId ==='none'){
+                selectedOptions = [];
+            }
+
+
             // 선택한 옵션이 이미 배열에 존재하면 제거, 그렇지 않으면 추가
-            if (selectedOptions.includes(optionId)) {
+            else if (selectedOptions.includes(optionId) && optionId !='none') {
                 selectedOptions = selectedOptions.filter(opt => opt !== optionId);
                 this.classList.remove('selected');
-            } else {
+            } 
+            else {
                 selectedOptions.push(optionId);
                 this.classList.add('selected');
             }
@@ -27,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // 선택된 옵션 표시 업데이트
             const selectedOptionDisplay = document.getElementById('selectedOptionDisplay');
-            if (selectedOptions.length > 0) {
+            if (selectedOptions.length > 0 && !selectedOptions.includes('none')) {
                 selectedOptionDisplay.textContent = `선택된 옵션은 ${selectedOptions.join(', ')} 입니다.`;
             } else {
                 selectedOptionDisplay.textContent = "선택된 옵션이 없습니다.";
@@ -40,11 +46,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if (selectedOptions.length > 0) {
             // 로컬스토리지에 선택한 옵션 저장
             localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
-
-            // 다음 페이지로 이동
-            window.location.href = 'v_askNum.html';
-        } else {
-            alert('옵션을 하나 이상 선택해주세요.');
         }
+
+        // 다음 페이지로 이동
+        window.location.href = 'v_askNum.html';
+         
     });
 });
